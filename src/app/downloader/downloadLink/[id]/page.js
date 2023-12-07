bb50e73c629e058ddfb49fb2ebbf1596c0e3af00
Hasnan <monirhasnan@gmail.com>
2023-12-08 03:13:39 +0600

    downloader updated

--- src/app/downloader/downloadLink/[id]/page.js
+++ src/app/downloader/downloadLink/[id]/page.js
@@ -1,13 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import ReuseableFetch from "@/ApiFetch/ReuseableFetch";
 import LoadingScreen from "@/components/LoadingScreen/LoadingScreen";
 
 const page = ({ params }) => {
-  const { data, isLoading } = ReuseableFetch(
+  const { data, isLoading, error } = ReuseableFetch(
     `https://vid.priv.au/api/v1/videos/${params?.id}`
   );
-  console.log(data);
+  console.log(error);
   return (
     <div>
       {isLoading ? (
@@ -16,28 +17,40 @@ const page = ({ params }) => {
         </>
       ) : (
         <>
-          <div className="grid place-items-center">
-            <h1 className="font-semibold mt-[10px]">{data?.title}</h1>
-            <div className="flex md:flex-row flex-col  gap-[10px] py-[5px]">
-              <a
-                className="text-center px-2 py-2 bg-red-500 rounded-md text-white hover:bg-red-600"
-                href={data?.formatStreams?.[2]?.url}
-                download={`${data?.title}.mp4`}
-                target="_blank"
-              >
-                Download Video
-              </a>
-              <a
-                className="text-center px-2 py-2 bg-green-500 rounded-md text-white hover:bg-green-600"
-                href={data?.adaptiveFormats?.[4]?.url}
-                type="audio"
-                download={`${data?.title}.mp3`}
-                target="_blank"
-              >
-                Download Audio
-              </a>
-            </div>
-          </div>
+          {data?.error ? (
+            <>
+              <div className="grid place-items-center">
+                <h1 className="text-red-600">
+                  Seems like you have not provided the correct link.
+                </h1>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="grid place-items-center">
+                <h1 className="font-semibold mt-[10px]">{data?.title}</h1>
+                <div className="flex md:flex-row flex-col  gap-[10px] py-[5px]">
+                  <a
+                    className="text-center px-2 py-2 bg-red-500 rounded-md text-white hover:bg-red-600"
+                    href={data?.formatStreams?.[2]?.url}
+                    download={`${data?.title}.mp4`}
+                    target="_blank"
+                  >
+                    Download Video
+                  </a>
+                  <a
+                    className="text-center px-2 py-2 bg-green-500 rounded-md text-white hover:bg-green-600"
+                    href={data?.adaptiveFormats?.[4]?.url}
+                    type="audio"
+                    download={`${data?.title}.mp3`}
+                    target="_blank"
+                  >
+                    Download Audio
+                  </a>
+                </div>
+              </div>
+            </>
+          )}
         </>
       )}
     </div>
--- src/components/DownloadVideoSection/DownloadVideoSection.jsx
+++ src/components/DownloadVideoSection/DownloadVideoSection.jsx
@@ -6,23 +6,22 @@ import { useEffect, useState } from "react";
 const DownloadVideoSection = () => {
   const [inputValue11, setInputValue11] = useState("");
   const [inputValue12, setInputValue12] = useState("");
-  const link_44 =
-    "https://www.youtube.com/watch?v=TUVcZfQe-Kw&list=RDONdviW7a6NA&index=4&pp=8AUB";
-  console.log(link_44.indexOf("v"));
   useEffect(() => {
-    if (inputValue11?.charAt(30) == "v") {
-      setInputValue12(inputValue11?.slice(32));
+    if (inputValue11?.charAt(30) == "v" && inputValue11?.length >= 78) {
+      let indexof_1 = inputValue11.indexOf("v");
+      let indexof_2 = inputValue11.indexOf("&");
+      setInputValue12(inputValue11?.slice(indexof_1 + 2, indexof_2));
+      console.log(inputValue12);
     } else if (inputValue11?.charAt(15) == "e") {
       let indexof_1 = inputValue11.indexOf("?");
       setInputValue12(inputValue11?.slice(17, indexof_1));
     } else if (inputValue11?.indexOf("v") == 28) {
       let indexof_1 = inputValue11.indexOf("v");
       setInputValue12(inputValue11?.slice(indexof_1 + 2));
-    } else if (inputValue11?.charAt(30) == "v" && inputValue11?.length > 60) {
-      let indexof_1 = inputValue11.indexOf("v");
-      let indexof_2 = inputValue11.indexOf("&");
-      setInputValue12(inputValue11?.slice(indexof_1 + 2, indexof_2));
-      console.log(inputValue12);
+    } else if (inputValue11?.charAt(30) == "v") {
+      setInputValue12(inputValue11?.slice(32));
+    } else {
+      setInputValue12(inputValue11);
     }
   }, [inputValue11, inputValue12]);
   return (
